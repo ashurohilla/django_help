ad8d7b5ec6c5e24c271f160efebd3c8df257d751
Ashish <90093173+ashurohilla@users.noreply.github.com>
2024-04-20 01:24:38 +0530

    adding  new vvales

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExampleConfig(AppConfig):
+class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'example'
+    name = 'posts'
